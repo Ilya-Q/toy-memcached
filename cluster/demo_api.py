8b13610a8b93a2df978445d0f799364b102333bb
oliver.koppenhoefer <oliver.koppenhoefer@mway.io>
2022-12-27 16:17:22 +0100

    multiple messages can be broadcast, added callback to receive broadcasts, removed reuse_port and switched 127.0.0.1 to 0.0.0.0 to make it work on my machine/network

--- cluster/demo_api.py
+++ cluster/demo_api.py
@@ -1,13 +1,29 @@
 import asyncio
 from broadcast import start_broadcast_protocol
 
+broadcast_protocol = None
+
+def broadcast_received(data):
+    print("woorks" + data)
+
+async def send_test_messages():
+    for i in range(3):
+        broadcast_protocol.broadcast("test " + str(i))
+        await asyncio.sleep(1)
+    broadcast_protocol.close()
+
 async def main():
-    await start_broadcast_protocol(
-        bind='127.0.0.1', 
+    global broadcast_protocol
+    broadcast_protocol, _, _ = await start_broadcast_protocol(
+        bind='0.0.0.0', 
         port=9000, 
-        broadcast_addr='192.168.1.255', 
-        broadcast_port=10001, 
-        broadcast_msg='This is a broadcast message')
+        broadcast_addr='192.168.178.255', 
+        broadcast_port=9000, 
+        broadcast_msg='This is a broadcast message',
+        data_received=broadcast_received)
+    asyncio.create_task(send_test_messages())
+    await asyncio.sleep(5)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
